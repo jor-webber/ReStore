a025a36bd4d23c2b74fa229cc15628a9d37396ba
jordan webber <webber_jordan_95@hotmail.com>
2021-12-20 11:35:29 -0500

    Added Routing

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from '@emotion/react';
+import { Route } from 'react-router';
+import HomePage from '../../features/home/HomePage';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +30,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog />
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/catalog' component={Catalog}/>
+        <Route exact path='/catalog/:id' component={ProductDetails}/>
+        <Route exact path='/about' component={AboutPage}/>
+        <Route exact path='/contact' component={ContactPage}/>
       </Container>
     </ThemeProvider>
   );
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Product } from "../../app/models/product";
 import { Avatar, CardHeader } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface Props {
   product: Product;
@@ -37,7 +38,7 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button size="small">Add to Cart</Button>
-        <Button size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
   );
